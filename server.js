f1843b65c30a601bb8d4720b2474c479d960974d
Raphael NGUEMA <raphaeljuniorsc@gmail.com>
2025-10-18 09:28:09 -0700

    Update 02

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ validateConfig();
 initializeFirebase();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Configuration EJS
 app.set('view engine', 'ejs');
